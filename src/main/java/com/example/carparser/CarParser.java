07abe16629bd1be871e042931b81cd263b59c1bb
Denis <sotnikovdenis04@gmail.com>
2022-11-02 00:03:20 +0500

    Add GetDescription function

--- src/main/java/com/example/carparser/CarParser.java
+++ src/main/java/com/example/carparser/CarParser.java
@@ -9,26 +9,56 @@ import java.util.ArrayList;
 public class CarParser {
     public static void main(String[] args) {
         try {
-            Document doc = Jsoup.connect("https://auto.drom.ru/mercedes-benz/g-class/").get();
+            Document document = Jsoup.connect("https://auto.drom.ru/mercedes-benz/g-class/?minprobeg=5000").get();
 
-            System.out.println(GetPrice(doc));
+            System.out.println(GetDescription(document));
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static ArrayList<Object> GetPrice(Document document) {
+    public static ArrayList<Integer> GetPrice(Document document) {
         var stringPrice = document.getElementsByAttributeValue("data-ftid", "bull_price");
-        var priceArray = new ArrayList<>();
+        var priceArray = new ArrayList<Integer>();
 
         for (Element elem : stringPrice) {
             var element = elem.toString().substring(29);
-            priceArray.add(Integer.parseInt(element.substring(0, element.length()-21)
+            priceArray.add(Integer.parseInt(element.substring(0, element.length() - 21)
                     .replace("&nbsp;", "")));
         }
 
         return priceArray;
+    }
+
+    public static ArrayList<String> GetCreatedDate(Document document) {
+        var stringDate = document.getElementsByAttributeValue("data-ftid", "bull_date").text();
+        var createdDateArray = new ArrayList<String>();
+
+        for (String word : stringDate.split("назад")) {
+            createdDateArray.add(word + "назад");
+        }
+
+        return createdDateArray;
+    }
+
+    public static ArrayList<String> GetDescription(Document document) {
+        var stringDescription = document.getElementsByAttributeValue("data-ftid", "component_inline-bull-description").text();
+        var arrayDescription = new ArrayList<String>();
+
+        var count = 0;
+        var beginString = 0;
+        for (var i = 0; i < stringDescription.length(); i++) {
+            if (String.valueOf(stringDescription.charAt(i)).equals(" ")) {
+                count++;
+                if (count == 10) {
+                    arrayDescription.add(stringDescription.substring(beginString, i).replace(",", ""));
+                    count = 0;
+                    beginString = i;
+                }
+            }
+        }
+
+        return arrayDescription;
     }
 }
 
