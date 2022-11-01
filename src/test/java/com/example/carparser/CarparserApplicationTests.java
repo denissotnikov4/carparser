2744c016c4a4b02c3d5fb51201e5e44f2fe8e8c2
Denis <sotnikovdenis04@gmail.com>
2022-11-01 21:24:51 +0500

    Add GetPrice function

---
+++ src/test/java/com/example/carparser/CarparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
